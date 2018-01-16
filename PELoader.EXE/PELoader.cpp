a4e70e3af457c01660fb64e94007ceb690c45b81
Noy Shmshoni <noy.sh22@gmail.com>
2018-01-16 15:11:26 +0000

    Add test dll no reloc file

--- PELoader.EXE/PELoader.cpp
+++ PELoader.EXE/PELoader.cpp
@@ -9,22 +9,31 @@
 #include <PELoader.h>
 #include "MemoryMappedFile.hpp"
 
+typedef DWORD (WINAPI* FUNC)(DWORD, DWORD);
+
 int main()
 {
-	
-	const std::wstring file(L"C:\\Windows\\System32\\user32.dll");
+	/*const std::wstring file(L"C:\\Windows\\System32\\user32.dll");
 
 	MemoryMappedFile mem_mapped_file(file);
 
 	const auto size = mem_mapped_file.get_file_buffer_size();
 
-	std::wcout << L"File size: " << size << std::endl;
+	std::wcout << L"File size: " << size << std::endl;*/
 
 	/*HLOADEDMODULE mod = Load();
 
 	Print(mod);
 
 	FreeLoadedModule(mod);*/
+
+	HMODULE mod = LoadLibrary(L"Z:\\projects\\peloader\\Output\\x64\\Debug\\TestDlls\\TestDLLNoReloc.dll");
+
+	FUNC f = (FUNC)GetProcAddress(mod, "Add");
+
+	DWORD val = f(1, 2);
+
+	std::wcout << val << std::endl;
 
     return 0;
 }
